097acca556db6c6aa0441adf8fe52f0440fc6ec5
Majid Shakeel <majidshakeelshawl@gmail.com>
2025-01-03 22:21:58 +0530

    feat: express initialization

---
+++ app.js
@@ -0,0 +1,20 @@
+import express from 'express';
+import os from 'os';
+
+const app = express();
+app.use(express.json());
+
+
+app.listen(3000, () => {
+    console.table({
+        status: 'running',
+        port: 3000,
+        time: new Date().toLocaleString(),
+        os: os.platform(),
+        host: os.hostname(),
+        cpus: os.cpus().length,
+        totalMemory: os.totalmem(),
+        freeMemory: os.freemem(),
+        uptime: os.uptime(),
+    })
+});
