da8c75b6e64a11ce856445efdcfbe3bd98e8f7cb
Majid Shakeel <majidshakeelshawl@gmail.com>
2025-01-03 22:30:42 +0530

    feat: env usage for logging

--- app.js
+++ app.js
@@ -3,18 +3,20 @@ import os from 'os';
 
 const app = express();
 app.use(express.json());
-console.log(process.env.ENVIRONMENT);
+
 
 app.listen(3000, () => {
-    console.table({
-        status: 'running',
-        port: 3000,
-        time: new Date().toLocaleString(),
-        os: os.platform(),
-        host: os.hostname(),
-        cpus: os.cpus().length,
-        totalMemory: os.totalmem(),
-        freeMemory: os.freemem(),
-        uptime: os.uptime(),
-    })
+    if (process.env.ENVIRONMENT === 'DEVELOPMENT') {
+        console.table({
+            status: 'running',
+            port: 3000,
+            time: new Date().toLocaleString(),
+            os: os.platform(),
+            host: os.hostname(),
+            cpus: os.cpus().length,
+            totalMemory: os.totalmem(),
+            freeMemory: os.freemem(),
+            uptime: os.uptime(),
+        }) 
+    }
 });
